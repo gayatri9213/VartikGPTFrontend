8fe49b269dddc1901c67f9d542c30b9318b49702
gayatri9213 <gayatri.sonar@aressindia.net>
2024-09-09 14:57:47 +0530

    update packages

--- src/components/LoginForm/msalConfig.js
+++ src/components/LoginForm/msalConfig.js
@@ -3,7 +3,7 @@ export const msalConfig = {
     clientId: "8f079aa4-4151-43be-ba66-0dbaf9b6b7a9",
     authority:
       "https://login.microsoftonline.com/da41fc73-c60d-4537-9111-22975cdbf183",
-    redirectUri: "http://vartikgpt.azurewebsites.net",
+    redirectUri: process.env.NEXT_PUBLIC_REDIRECT_URI,
   },
   cache: {
     cacheLocation: "localStorage",
