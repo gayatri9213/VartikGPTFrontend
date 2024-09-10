3e4b9eafaaa07e903c5290c0a3a895f8e1da8bf9
gayatri9213 <gayatri.sonar@aressindia.net>
2024-09-10 10:32:49 +0530

    deployment

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
    output: 'export', // This enables static export
+   images: {
+    unoptimized: true, // Disable Image Optimization for static export
+  },
 };
 
 export default nextConfig;
