68d8cb09beec9cc57dba120056171b430a3dc543
gayatri9213 <gayatri.sonar@aressindia.net>
2024-09-11 15:35:32 +0530

    External API change

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export', // This enables static export
-  images: {
-    unoptimized: true, // Disable Image Optimization for static export
-  },
+  // output: 'export', // This enables static export
+  // images: {
+  //   unoptimized: true, // Disable Image Optimization for static export
+  // },
   env: {
     REACT_APP_MSAL_REDIRECT_URI:process.env.REACT_APP_MSAL_REDIRECT_URI,
     REACT_APP_API_BASE_URL:process.env.REACT_APP_API_BASE_URL,
